e600b76da6739d0f6ce93b9735937f97bbfda799
Heli Eronen <herojonen@gmail.com>
2022-02-13 18:41:37 +0200

    Osa3 tehtävä 3.22 lint+korjaukset, numeron muokkauksen validointi

--- models/person.js
+++ models/person.js
@@ -5,46 +5,46 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then( result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        const parts = v.split('-')
+        if (parts.length > 2) {
+          return false
+        } else if (parts[0].length > 3 || parts[0].length < 2) {
+          return false
+        } else if (parts[1].length < 5) {
+          return false
+        }
+        return true
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function(v) {
-                const parts = v.split('-')
-                if (parts.length > 2) {
-                    return false
-                } else if (parts[0].length > 3 || parts[0].length < 2) {
-                    return false
-                } else if (parts[1].length < 5) {
-                    return false
-                }
-                return true
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        minlength: 8
-    }
+    minlength: 8
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
